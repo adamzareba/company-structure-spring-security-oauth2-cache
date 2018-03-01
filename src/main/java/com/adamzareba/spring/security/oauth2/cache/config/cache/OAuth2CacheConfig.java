57b870a8c9aa64098588123ba23d0a0aef63aad1
Adam Zaręba <bontade88@gmail.com>
2018-03-01 13:14:34 +0100

    ehcache config

--- src/main/java/com/adamzareba/spring/security/oauth2/cache/config/cache/OAuth2CacheConfig.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/cache/config/cache/OAuth2CacheConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
-@ImportResource({"classpath:oauth2-ehcache.xml"})
+@ImportResource({"classpath:oauth2-cache.xml"})
 public class OAuth2CacheConfig {
 
 }
