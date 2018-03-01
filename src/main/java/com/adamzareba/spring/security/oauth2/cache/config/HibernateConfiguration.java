bb74aa72135db430444281984d9d929fe303e069
Adam Zaręba <bontade88@gmail.com>
2018-03-01 13:11:36 +0100

    redis config

--- src/main/java/com/adamzareba/spring/security/oauth2/cache/config/HibernateConfiguration.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/cache/config/HibernateConfiguration.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
--- src/main/java/com/adamzareba/spring/security/oauth2/cache/config/cache/CacheConfiguration.java
+++ src/main/java/com/adamzareba/spring/security/oauth2/cache/config/cache/CacheConfiguration.java
@@ -1,31 +1,49 @@
 package com.adamzareba.spring.security.oauth2.cache.config.cache;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
+@PropertySource("classpath:/redis.properties")
 public class CacheConfiguration {
 
-    @Bean
-    public EhCacheManagerFactoryBean ehCacheManagerFactory() {
-        EhCacheManagerFactoryBean cacheManagerFactoryBean = new EhCacheManagerFactoryBean();
-        cacheManagerFactoryBean.setConfigLocation(new ClassPathResource("ehcache.xml"));
-        cacheManagerFactoryBean.setShared(true);
+    @Value("${redis.host}")
+    private String redisHost;
 
-        return cacheManagerFactoryBean;
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory() {
+        JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+        redisConnectionFactory.setHostName(redisHost);
+        redisConnectionFactory.setPort(redisPort);
+
+        return redisConnectionFactory;
     }
 
     @Bean
-    public EhCacheCacheManager ehCacheCacheManager() {
-        EhCacheCacheManager cacheManager = new EhCacheCacheManager();
-        cacheManager.setCacheManager(ehCacheManagerFactory().getObject());
-        cacheManager.setTransactionAware(true);
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<?, ?> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate());
+        cacheManager.setUsePrefix(true);
+        cacheManager.setDefaultExpiration(240);
 
         return cacheManager;
     }
+
 }
